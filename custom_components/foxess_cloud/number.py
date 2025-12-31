52201a16af9b505c28ca1e146f7caeb3a4a6e387
Nick Whyte <nick@nickwhyte.com>
2025-12-31 23:36:57 +1100

    Introduce linting in CI (#5)

--- custom_components/foxess_cloud/number.py
+++ custom_components/foxess_cloud/number.py
@@ -13,7 +13,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .api_client import FoxESSCloudClient
-from .const import CONF_DEVICE_SN, DOMAIN
+from .const import CONF_DEVICE_SN
 from .coordinator import FoxESSCloudSchedulerCoordinator
 
 SCAN_INTERVAL = timedelta(hours=1)
